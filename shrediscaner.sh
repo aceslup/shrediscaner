24f62af70c0737f2ea87725d4d2e4b7fb4bfe93f
Aceslup <aceslup@users.noreply.github.com>
2023-06-08 09:40:42 +0800

    Update shrediscaner.sh

--- shrediscaner.sh
+++ shrediscaner.sh
@@ -13,12 +13,12 @@ Usage:
   $0 <Host> <Port> <ACT> <Pattern> [pass]
 
 ACT Options:
-  expire	# 设置失效时间
-  unlink	# 异步清理
+  expire  # 设置失效时间 (1-9s随机)
+  unlink  # 异步清理
   del     # 同步清理
 
 Example:
- ./shrediscaner.sh 127.0.0.1 6379 DEL "prefix_*" mypass
+  ./$0 127.0.0.1 6379 DEL "prefix_*" mypass
 
 EOF
     exit 1
